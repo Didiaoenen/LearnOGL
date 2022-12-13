2efe44a068304c7d987f8e346a263094b4860ef2
Didiaoenen <1191430879@qq.com>
2022-12-13 23:31:54 +0800

    add commit

--- libs/VS2022/LearnOGL/LearnOGLPipeline.cpp
+++ libs/VS2022/LearnOGL/LearnOGLPipeline.cpp
@@ -1,9 +1,63 @@
 #include "LearnOGLPipeline.h"
 
-LearnOGLPipeline::LearnOGLPipeline()
+namespace OGL
 {
-}
+	LearnOGLPipeline::LearnOGLPipeline() :
+		mPos(glm::vec3(0.0f)),
+		mScale(glm::vec3(1.0f)),
+		mRotate(glm::vec3(0.0f)),
+		mPersInfo(),
+		mOrthoInfo()
+	{
 
-LearnOGLPipeline::~LearnOGLPipeline()
-{
+	}
+
+	LearnOGLPipeline::~LearnOGLPipeline()
+	{
+	}
+
+	void LearnOGLPipeline::SetPos(float x, float y, float z)
+	{
+		mPos.x = x;
+		mPos.y = y;
+		mPos.z = z;
+	}
+
+	void LearnOGLPipeline::SetScale(float x, float y, float z)
+	{
+		mScale.x = x;
+		mScale.y = y;
+		mScale.z = z;
+	}
+
+	void LearnOGLPipeline::SetRotate(float x, float y, float z)
+	{
+		mRotate.x = x;
+		mRotate.y = y;
+		mRotate.z = z;
+	}
+
+	void LearnOGLPipeline::SetCamera(const LearnOGLCamera& camera)
+	{
+	}
+
+	void LearnOGLPipeline::SetPerspectiveInfo(const PersProjInfo& info)
+	{
+		mPersInfo = info;
+	}
+
+	void LearnOGLPipeline::SetOrthographicInfo(const OrthoProjInfo& info)
+	{
+		mOrthoInfo = info;
+	}
+
+	glm::mat4 LearnOGLPipeline::GetPerspectiveProjection()
+	{
+		return glm::mat4();
+	}
+
+	glm::mat4 LearnOGLPipeline::GetOrthographicProjection()
+	{
+		return glm::mat4();
+	}
 }
--- libs/VS2022/LearnOGL/LearnOGLPipeline.h
+++ libs/VS2022/LearnOGL/LearnOGLPipeline.h
@@ -1,12 +1,38 @@
 #pragma once
 
-class LearnOGLPipeline
+#include <glm/glm.hpp>
+
+#include "LearnOGLApp.h"
+#include "LearnOGLCamera.h"
+
+namespace OGL
 {
-public:
-	LearnOGLPipeline();
-	~LearnOGLPipeline();
+	class LearnOGLPipeline
+	{
+	public:
+		LearnOGLPipeline();
+		~LearnOGLPipeline();
 
-private:
 
-};
+		void SetPos(float x, float y, float z);
+		void SetScale(float x, float y, float z);
+		void SetRotate(float x, float y, float z);
 
+		void SetCamera(const LearnOGLCamera& camera);
+		void SetPerspectiveInfo(const PersProjInfo& info);
+		void SetOrthographicInfo(const OrthoProjInfo& info);
+
+		glm::mat4 GetPerspectiveProjection();
+		glm::mat4 GetOrthographicProjection();
+
+	private:
+		glm::vec3 mPos;
+		glm::vec3 mScale;
+		glm::vec3 mRotate;
+
+		LearnOGLCamera mCamera;
+
+		PersProjInfo mPersInfo;
+		OrthoProjInfo mOrthoInfo;
+	};
+}
