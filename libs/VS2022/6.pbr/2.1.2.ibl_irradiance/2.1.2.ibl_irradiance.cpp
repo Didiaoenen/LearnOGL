4a09db7ab43dacd72f92a9fbeadc11fe6d8108e9
Didiaoenen <1191430879@qq.com>
2023-02-07 21:02:29 +0800

    add commit

---
+++ libs/VS2022/6.pbr/2.1.2.ibl_irradiance/2.1.2.ibl_irradiance.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <LearnOGLApp.h>
+
+class ibl_irradiance : public OGL::LearnOGLApp
+{
+public:
+	virtual bool Init() override
+	{
+		OGL::LearnOGLApp::Init();
+
+		mPersInfo.fov = 60.0f;
+		mPersInfo.width = info.windowWidth;
+		mPersInfo.height = info.windowHeight;
+		mPersInfo.zFar = 100.0f;
+		mPersInfo.zNear = 0.1f;
+
+		return true;
+	}
+
+	virtual void Setup() override
+	{
+
+	}
+
+	virtual void Update(double dt) override
+	{
+
+	}
+
+	virtual void Input() override
+	{
+
+	}
+
+	virtual void Render(OGL::LearnOGLContext* context) override
+	{
+
+	}
+
+	virtual void ShutDown() override
+	{
+
+	}
+
+private:
+	OGL::LearnOGLShader* mPBRShader;
+	OGL::LearnOGLShader* mEquirectangularToCubemapShader;
+	OGL::LearnOGLShader* mIrradianceShader;
+	OGL::LearnOGLShader* mBackgroundShader;
+
+
+
+};
