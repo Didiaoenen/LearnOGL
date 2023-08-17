2fbe79a81cdaa5e51366ffa1ab0bd663ef9e804c
Didiaoenen <1191430879@qq.com>
2023-08-18 00:17:12 +0800

    commit add

--- libs/VS2022/LearnOGL/SceneObjectCamera.cpp
+++ libs/VS2022/LearnOGL/SceneObjectCamera.cpp
@@ -13,13 +13,13 @@ const glm::mat4& SceneObjectCamera::GetProjectionMatrix()
 	{
 		auto width = mOrthographicWidth / 2;
 		auto height = mOrthographicHeight / 2;
-		return glm::ortho(-width, width, -height, height, mClipPlaneNear, mClipPlaneFar);
+		mProjectionMatrix = glm::ortho(-width, width, -height, height, mClipPlaneNear, mClipPlaneFar);
 	}
 	else if (mCameraType == _CameraType::PERSPECTIVE)
 	{
-		return glm::perspective(glm::radians(mHorizontalFOV), mOrthographicWidth / mOrthographicHeight, mClipPlaneNear, mClipPlaneFar);
+		mProjectionMatrix = glm::perspective(glm::radians(mHorizontalFOV), mOrthographicWidth / mOrthographicHeight, mClipPlaneNear, mClipPlaneFar);
 	}
-	return glm::mat4(1.0);
+	return mProjectionMatrix;
 }
 
 const glm::mat4& SceneObjectCamera::GetViewProjectionMatrix()
